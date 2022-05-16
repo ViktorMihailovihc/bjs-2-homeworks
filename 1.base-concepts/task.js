d78b808c22944c5bba2f5ecf5a5e645240dd1b2b
ViktorMihailovihc <9600141738@yandex.ru>
2022-05-16 17:15:02 +0300

    change task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,20 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b**2-4*a*c;
+  let x1 = "";
+  let x2 = "";
+  
+  if (d === 0) { x1 = -b/(2*a); arr.push(x1); console.log(arr); 
+  } else 
+  if (d < 0) {console.log(arr);
+  } else
+  if (d > 0) {x1 = (-b + Math.sqrt(d) )/(2*a);
+x2 = (-b - Math.sqrt(d) )/(2*a); arr.push(x1,x2); console.log(arr);}
+  return arr; 
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
