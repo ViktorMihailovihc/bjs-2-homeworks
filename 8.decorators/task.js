e5efba7d0dd46e2622586a5d730a7f173af7f175
ViktorMihailovihc <9600141738@yandex.ru>
2022-06-15 09:33:24 +0300

    added task,js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,60 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = [];
+
+  return (...args) => {
+    const hash = args.join(',');
+    const index = cache.findIndex((item) => item.hash === hash);
+    let result;
+
+    if (index !== -1) {
+      result = 'Из кэша: ' + cache[index].value;
+    } else {
+      const value = func(...args);
+      cache.push({ hash, value });
+      if (cache.length > 5) { 
+        cache.shift();
+      }
+      result = 'Вычисляем: ' + value; 
+    }
+
+    console.log(result);
+    return result;
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, time) {
+  let flag = false;
+  let timerId;
+
+  return (...args) => {
+    clearTimeout(timerId);
+
+    if (!flag) {
+      func(...args);
+      flag = true;
+    }
+
+    timerId = setTimeout(() => flag = false, 2000);
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, time) {
+  let flag = false;
+  let timerId;
+
+  const wrapper = (...args) => {
+    clearTimeout(timerId);
+
+    if (!flag) {
+      func(...args);
+      wrapper.count++;
+      flag = true;
+    }
+
+    timerId = setTimeout(() => flag = false, time);
+  }
+
+  wrapper.count = 0;
+  return wrapper;
 }
